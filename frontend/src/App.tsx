959941805f295f971297407ec65e3598c92b12c2
admstud <admstud@MX1-Docker-18.EDU-WSB.local>
2026-03-28 14:35:40 -0100

    Artefakt6

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,11 @@
 import Dashboard from './pages/Dashboard';
-import './App.css';
- 
+
 function App() {
   return (
-<div className="App">
-      {/* To sprawi, że Dashboard pojawi się na stronie */}
-<Dashboard />
-</div>
+    <div>
+      <Dashboard />
+    </div>
   );
 }
- 
+
 export default App;
--- frontend/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
- 
-const api = axios.create({
-// Vite wymaga prefixu import.meta.env dla zmiennych z .env
-  baseURL: import.meta.env.VITE_API_URL,
-});
- 
-export default api;
